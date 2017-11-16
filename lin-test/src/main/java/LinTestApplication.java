d460a6c0f4228b93e63535a83814cb8919374ec3
huluhaziqi <huluhaziqi@gmail.com>
2017-11-16 22:55:57 +0800

    first commit

---
+++ lin-test/src/main/java/LinTestApplication.java
@@ -0,0 +1,12 @@
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class LinTestApplication {
+    private static Logger logger = LoggerFactory.getLogger(LinTestApplication.class);
+    public static void main(String[] args) {
+        logger.info("start lin test");
+        new ClassPathXmlApplicationContext("classpath:META-INF/spring/*.xml");
+        logger.info("end lin test");
+    }
+}
---
+++ lin-test/src/main/java/base/BaseService.java
@@ -0,0 +1,9 @@
+package base;
+
+public interface BaseService<Record, Example> {
+    int countByExample(Example example);
+
+    Record selectByPrimaryKey(Example example);
+
+    void initMapper();
+}
